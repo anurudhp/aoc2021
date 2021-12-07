ef40d5a6f75ced9fa198643ec2fcb9c16fb20cb2
anurudhp <anurudhp97@gmail.com>
2021-12-07 15:09:34 +0530

    day 7: fix bug in expression

--- day07.R
+++ day07.R
@@ -6,7 +6,7 @@ crabs <- sapply(read.csv(inf, header=FALSE), as.integer)
 sum(abs(crabs - median(crabs)))
 
 # part 2
-xopt <- mean(crabs) - 0.5
+xopt <- mean(crabs)
 min(sapply(c(floor(xopt), ceiling(xopt)),
            function(x) { d <- abs(crabs - x); sum((d**2 + d)/2) }))
 
